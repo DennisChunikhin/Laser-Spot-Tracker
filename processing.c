3e1977759fe0e2d9afa8f0b11adaef6fe977dbcc
DennisChunikhin <dennis.chunikhin@gmail.com>
2024-06-07 15:39:38 -0400

    Implemented basic processing

--- processing.c
+++ processing.c
@@ -4,24 +4,54 @@
 
 // TODO: Implement this
 int getBeamProperties(const unsigned char *imgBuf, long long int sizeX, long long int sizeY, struct beamProperties *p) {
-    // TEMP
-    p->xAvg = 0;
-    p->yAvg = 0;
-    
+    // Brightest pixel
     unsigned char max = 0;
-    int maxIndex = 0;
+    int xMax = 0, yMax = 0;
+
+    // First moment
+    double EX = 0, EY = 0;
     
-    for (int i=0; i < sizeX * sizeY; i++) {
-        // Find brightest pixel
-        unsigned char val = imgBuf[i];
-        if (val > max) {
-            max = val;
-            maxIndex = i;
+    // Second moment
+    double EX2 = 0, EY2 = 0;
+
+    int i=0;
+    for (int row=0; row < sizeY; row++) {
+        for (int col=0; col < sizeX; col++) {
+            // Find brightest pixel
+            unsigned char val = imgBuf[i];
+            if (val > max) {
+                max = val;
+                xMax = col;
+                yMax = row;
+            }
+
+            // Measure mean and standard deviation
+            double intensity = (double)val;
+            double EX_term = intensity*row, EY_term = intensity*col;
+            EX += EX_term;
+            EY += EY_term;
+            EX2 += EX_term * row;
+            EY2 += EY_term * col;
+
+            i++;
         }
     }
+
+    long long int N = sizeX * sizeY;
+    EX /= N;
+    EY /= N;
+    EX2 /= N;
+    EY2 /= N;
+
+    p->xAvg = EX;
+    p->yAvg = EY;
+    p->xStd = EX2 - EX*EX;
+    p->yStd = EY2 - EY*EY;
+
+    p->xMax = xMax;
+    p->yMax = yMax;
     
-    printf("%d\n",maxIndex);
-    UNWRAP(maxIndex, sizeX, p->xMax, p->yMax);
+    //UNWRAP(maxIndex, sizeX, p->xMax, p->yMax);
     
     return 0;
 }
